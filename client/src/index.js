0d08b711bb2e3c1d88aacb4bcef8ae885868b439
Yolxi <jacacanada@gmail.com>
2020-08-25 23:16:41 -0400

    finall commit before deployment

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,7 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
 const wsLink = new WebSocketLink({
   uri: 'ws://localhost:4000/graphql',
   options: {
